1f51d7de289f99a6bc969efbe1b05da864c380d6
Yehiyam Livneh <yehiyam@gmail.com>
2019-10-28 10:56:45 +0200

    remove python/node-env from hard-coded list

--- images.js
+++ images.js
@@ -1,18 +1,18 @@
 const coreImages = [
-    {
-        image:{
-            repository:'hkube/python-env',
-            tag:'1.1.88'
-        },
-        name: 'python-env'
-    },
-    {
-        image:{
-            repository:'hkube/nodejs-env',
-            tag:'1.1.84'
-        },
-        name: 'nodejs-env'
-    },
+    // {
+    //     image:{
+    //         repository:'hkube/python-env',
+    //         tag:'1.1.88'
+    //     },
+    //     name: 'python-env'
+    // },
+    // {
+    //     image:{
+    //         repository:'hkube/nodejs-env',
+    //         tag:'1.1.84'
+    //     },
+    //     name: 'nodejs-env'
+    // },
     {
         image:{
             repository:'hkube/base-node',

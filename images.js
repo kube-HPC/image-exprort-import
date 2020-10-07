06d585e3f4c130af96de60af3ca2c57b393108c4
Yehiyam Livneh <yehiyam@gmail.com>
2020-10-07 18:12:39 +0300

    download base images

--- images.js
+++ images.js
@@ -16,9 +16,30 @@ const coreImages = [
     {
         image:{
             repository:'hkube/base-node',
-            tag:'v1.1.1'
+            tag:'v1.2.0'
         },
-        name: 'base-node'
+        name: 'base-node',
+    },
+    {
+        image:{
+            repository:'python',
+            tag:'3.7'
+        },
+        name: 'python37'
+    },
+    {
+        image:{
+            repository:'node',
+            tag:'14.5.0-slim'
+        },
+        name: 'node14'
+    },
+    {
+        image:{
+            repository:'adoptopenjdk/openjdk11',
+            tag:'latest'
+        },
+        name: 'node14'
     }
 ]
 module.exports={

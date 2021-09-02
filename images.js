1fbc92a8b00cd02b7d5ff9fbec11b4b4d4aa7a01
yehiyam <yehiyam@users.noreply.github.com>
2021-09-02 15:55:40 +0300

    add fluent docker

--- images.js
+++ images.js
@@ -14,41 +14,55 @@ const coreImages = [
     //     name: 'nodejs-env'
     // },
     {
-        image:{
-            repository:'hkube/base-node',
-            tag:'v1.2.0'
+        image: {
+            repository: 'hkube/base-node',
+            tag: 'v1.2.0'
         },
         name: 'base-node',
     },
     {
-        image:{
-            repository:'python',
-            tag:'3.7'
+        image: {
+            repository: 'python',
+            tag: '3.7'
         },
         name: 'python37'
     },
     {
-        image:{
-            repository:'node',
-            tag:'14.5.0-slim'
+        image: {
+            repository: 'node',
+            tag: '14.5.0-slim'
         },
         name: 'node14'
     },
     {
-        image:{
-            repository:'hkube/openjdk11',
-            tag:'jre-11.0.8_10-ubuntu'
+        image: {
+            repository: 'node',
+            tag: '14.5.0'
+        },
+        name: 'node14full'
+    },
+    {
+        image: {
+            repository: 'hkube/openjdk11',
+            tag: 'jre-11.0.8_10-ubuntu'
         },
         name: 'java'
     },
     {
-        image:{
-            repository:'maven',
-            tag:'3.6.3-openjdk-11-slim'
+        image: {
+            repository: 'maven',
+            tag: '3.6.3-openjdk-11-slim'
         },
         name: 'maven'
+    },
+    {
+        image: {
+            repository: 'fluent/fluent-bit',
+            tag: '1.8.3'
+        },
+        name: fluentbit
     }
 ]
-module.exports={
+module.exports = {
     coreImages
 }
